54684b42936609ecebd1b4405297043071c40db4
Moknine Elhadj <mokninemoknine999@gmail.com>
2024-04-06 14:58:12 +0200

    connect local database to kinde webhook

--- app/components/SignInPanal.tsx
+++ app/components/SignInPanal.tsx
@@ -1,6 +1,7 @@
 import {
   getKindeServerSession,
   LoginLink,
+  LogoutLink,
   RegisterLink,
 } from "@kinde-oss/kinde-auth-nextjs/server";
 import React from "react";
@@ -9,7 +10,12 @@ import { Button } from "@nextui-org/button";
 const SignInPanal = async () => {
   const { isAuthenticated, getUser } = await getKindeServerSession();
   const user = await getUser();
-  if (await isAuthenticated()) return <div>{user?.given_name}</div>;
+  if (await isAuthenticated())
+    return (
+      <div>
+        {user?.given_name} <LogoutLink>LogOut</LogoutLink>
+      </div>
+    );
   return (
     <div className=" flex gap-3 items-center">
       <Button color="primary">
